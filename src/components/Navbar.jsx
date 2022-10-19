050921b217fc43812e96e62d3de859536b5ac63f
bsaly <b.saly84@gmail.com>
2022-10-19 17:44:48 +0200

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <li><a href="" className="title">Branislav Šály</a></li>
+            </div>
+            <div className="header">
+                <ul className="menu">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">CV</a></li>
+                    <li><a href="">My Story</a></li>
+                </ul>
+            </div>
+        </nav >
+    )
+}
+
+export default Navbar
