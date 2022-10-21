be7824db0cbb6255e67c8e02365a52147b57203e
bsaly <b.saly84@gmail.com>
2022-10-21 16:15:05 +0200

    Add typing

--- dennik.md
+++ dennik.md
@@ -1,13 +1,20 @@
 ## CSS
 - aby som mohol vo flexboxe pouzit aj `align-items: center;`, tak potrebujem aj urcit vysku napr. `100vh`
+- position absolute mi robi ze mozem hybat objektom bez toho, aby mi robili niejake hranice a moze sa prekrivat
 
 ### footer
 - vdaka tomuto bude footer stale dole
 ```CS
 display: flex;
     justify-content: center;
-    position: absolute;
+    position: relative;
+    // alebo absolute
     bottom: 0;
     width: 99.9%;
     height: 2.5rem; 
 ```
+### z-index
+- urcuje prioritu ze co moze co prekrit
+
+## React
+- npm config set legacy-peer-deps true - velmi uzitocne, odstranuje errory ked chcem npm i nieco
--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,12 +1,32 @@
+import portrait from './imgs/portrait.jpg'
+import Typewriter from 'typewriter-effect';
+
+
 const Frontpage = () => {
     return (
         <div className="maintitle">
-
-            <div></div>
+            <div id="mainphoto">
+                <img src={portrait} alt="portrait" />
+            </div>
             <h1>
-                Hello, I'm <strong>Branislav Šály</strong>, an unemployd man who is <br />
-                searching for junior web development job in Bratislava.
+                Hello, I'm <strong>Branislav Šály</strong>, Self-thought Web
+                Developer, <br />Who is Searching Junior Front-end Job in Bratislava.
             </h1>
+            <div className='typing-effect'>
+                <Typewriter
+                    options={{
+                        strings: [
+                            '<strong>Hire me</strong> please',
+                            'I will learn <strong>anything</strong>',
+                        ],
+                        cursor: [''],
+                        deleteSpeed: 40,
+                        autoStart: true,
+                        loop: true,
+                        pauseFor: 2000,
+                    }}
+                />
+            </div>
         </div>
     )
 }
